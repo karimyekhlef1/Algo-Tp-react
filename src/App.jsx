b4a909a84dbf5d7a0839caf0b500ed0f2a3a9508
karim yekhlef <76750881+karimyekhlef1@users.noreply.github.com>
2023-11-14 22:24:55 +0100

    0.0.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Flow from './component/Flow'
 import GetData from './api/GetData'
-import Test from './component/test'
-import Vehicles from './component/Vehicles'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    {/* <Vehicles/> */}
      <GetData/>
 
     </>
